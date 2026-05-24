7a63f05005c442d6f1d6afa732e21cbaa8bbdc82
ziad-wdev <ziadahmed2371@gmail.com>
2026-05-24 04:46:48 +0300

    main

--- home/default/modules/waybar.nix
+++ home/default/modules/waybar.nix
@@ -191,7 +191,6 @@ in
         "hyprland/workspaces" = {
           format = "{icon}";
           all-outputs = true;
-          on-click = "hyprctl dispatch 'hl.dsp.focus({ workspace = {id} })'";
           persistent-workspaces = {
             "*" = [ 1 2 3 4 5 ];
           };
